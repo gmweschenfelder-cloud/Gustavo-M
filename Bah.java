1db395749554fd8e34e1d5b7a1cbaaa4045f9c28
Gustavo <gmweschenfelder@gmail.com>
2025-10-30 17:07:56 -0300

    ninooii

---
+++ Bah.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Bah{
+    public final  static  Scanner TECLADO =  new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+
+    }
+    
+}
